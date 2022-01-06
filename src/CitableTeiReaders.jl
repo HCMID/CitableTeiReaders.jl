d3a0f6cf02716873c3c626c3c89f3d9810ef9057
Neel Smith <dnsmith.neel@gmail.com>
2022-01-06 17:15:50 -0500

    Implement new API

--- src/CitableTeiReaders.jl
+++ src/CitableTeiReaders.jl
@@ -1,7 +1,10 @@
 module CitableTeiReaders
-
+using CitableBase
 using CitableText, CitableCorpus, EzXML
 using Documenter, DocStringExtensions
+
+
+#export citestructuretrait
 
 export ezxmlstring
 export twocolumnReader
@@ -9,9 +12,17 @@ export simpleAbReader, poeticLineReader
 export divAbReader, divLineReader
 export threeDivReader, groupedThreeDivReader
 
+export readcitable
+export TEIPoeticLine
+
+include("generic.jl")
+include("poeticLineReader.jl")
+
+
+
 include("xmlutilities.jl")
 include("simpleAbReader.jl")
-include("poeticLineReader.jl")
+
 include("twoTierReaders.jl")
 include("threeTierReaders.jl")
 include("twocolumnReader.jl")
--- src/poeticLineReader.jl
+++ src/poeticLineReader.jl
@@ -1,4 +1,4 @@
-
+#=
 """
 $(SIGNATURES)
 Read a TEI document with 1-tier citation values on `/TEI/text/body/l` elements.
@@ -11,3 +11,17 @@ function poeticLineReader(xml::AbstractString, urnBase::CtsUrn)::CitableTextCorp
     CitableTextCorpus(cnodes)
 end
 
+=#
+
+
+"Singleton type for TEI document cited by poetic line."
+struct TEIPoeticLine <: CiteStructureTrait end
+
+"Implementation of `readcitable` for type `TEIPoeticLine`."
+function readcitable(src::AbstractString, urn::CtsUrn, rdr::Type{TEIPoeticLine})
+    doc = parsexml(src)
+    xp = "/ns:TEI/ns:text/ns:body/ns:l"
+    blocks = findall(xp, doc.root,["ns"=> teins])
+    cnodes = map(b -> citeNAttr(b, urn), blocks)
+    CitableTextCorpus(cnodes)
+end
